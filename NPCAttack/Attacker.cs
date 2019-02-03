44fa2fd0972a6db8df20da668a575670ca5f9b3e
Charles Kenney <charlesc.kenney@gmail.com>
2019-02-02 21:42:09 -0500

    init

---
+++ NPCAttack/Attacker.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms;
+using GTA;
+using GTA.Math;
+
+
+namespace NPCAttack
+{
+    /// <summary>
+    /// Attacker
+    /// </summary>
+    public class Attacker : Script
+    {
+        public Attacker()
+        {
+            this.KeyDown += onKeyDown;
+        }
+
+        private void onKeyDown(object sender, KeyEventArgs evt)
+        {
+            if (evt.KeyCode == Keys.F4)
+            {
+                var npc = World.CreatePed(GTA.Native.PedHash.Abigail, Game.Player.Character.GetOffsetInWorldCoords(new Vector3(0, 5, 0)));
+                npc.Weapons.Give(GTA.Native.WeaponHash.RPG, 5, true, true);
+                npc.Task.FightAgainst(Game.Player.Character);
+            }
+        }
+    }
+}
